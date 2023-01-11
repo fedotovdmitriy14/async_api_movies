80aad7fb90e171412bfe23c763252a7876790b64
Александр Карулин <av.karulin@tensor.ru>
2023-01-12 00:24:44 +0300

    model and services

--- fastapi-solution/main.py
+++ fastapi-solution/main.py
@@ -1,7 +1,7 @@
 import logging
 
 import aioredis
-import uvicorn as uvicorn
+import uvicorn
 from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
@@ -21,25 +21,24 @@ app = FastAPI(
 
 @app.on_event('startup')
 async def startup():
-    # Подключаемся к базам при старте сервера
-    # Подключиться можем при работающем event-loop
-    # Поэтому логика подключения происходит в асинхронной функции
     redis.redis = await aioredis.create_redis_pool((config.REDIS_HOST, config.REDIS_PORT), minsize=10, maxsize=20)
     elastic.es = AsyncElasticsearch(hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}'])
 
 
 @app.on_event('shutdown')
 async def shutdown():
-    # Отключаемся от баз при выключении сервера
-    await redis.redis.close()
+    redis.redis.close()
+    await redis.redis.wait_closed()
     await elastic.es.close()
 
+
+# Подключаем роутер к серверу, указав префикс /v1/films
+# Теги указываем для удобства навигации по документации
+app.include_router(films.router, prefix='/api/v1/films', tags=['films'])
 
 if __name__ == '__main__':
     uvicorn.run(
         'main:app',
         host='0.0.0.0',
         port=8000,
-        log_config=LOGGING,
-        log_level=logging.DEBUG,
     )
