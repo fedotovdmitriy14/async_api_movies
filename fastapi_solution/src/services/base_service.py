61ed6d68511e9820d2e0aa30cb91349ebd24910f
Александр Карулин <av.karulin@tensor.ru>
2023-01-20 23:57:41 +0300

    search in elastic

--- fastapi_solution/src/services/base_service.py
+++ fastapi_solution/src/services/base_service.py
@@ -9,9 +9,10 @@ ESIndex = (Genre, Person)
 
 
 class BaseService:
-    def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
+    def __init__(self, redis: Redis, elastic: AsyncElasticsearch, index: str):
         self.redis = redis
         self.elastic = elastic
+        self.index = index
 
     async def _data_from_cache(self, data_id: str, index: ESIndex) -> Optional[Union[ESIndex]]:
         data = await self.redis.get(data_id)
@@ -27,3 +28,10 @@ class BaseService:
         # https://redis.io/commands/set
         # pydantic позволяет сериализовать модель в json
         await self.redis.set(key, value, expire=CACHE_EXPIRE_IN_SECONDS)
+
+    async def search_in_elastic(self, body: dict, schema: ESIndex) -> Optional[Union[ESIndex]]:
+        try:
+            docs = await self.elastic.search(index=self.index, body=body)
+            return [schema(**row["_source"]) for row in docs["hits"]["hits"]]
+        except NotFoundError:
+            return None
