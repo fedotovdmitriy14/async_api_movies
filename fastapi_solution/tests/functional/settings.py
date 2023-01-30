2a0a5ceb9f6f7897d7c7fc67dbac1b2756910f4a
Александр Карулин <av.karulin@tensor.ru>
2023-01-31 00:17:33 +0300

    async test search

--- fastapi_solution/tests/functional/settings.py
+++ fastapi_solution/tests/functional/settings.py
@@ -1,19 +1,15 @@
-import os
 from pydantic import BaseSettings, Field
 
 
 class TestSettings(BaseSettings):
-    project_name: str = Field(..., env="PROJECT_NAME")
-    redis_host: str = Field(..., env="REDIS_HOST")
-    redis_port: str = Field(..., env="REDIS_PORT")
-    elastic_host: str = Field(..., env="ELASTIC_HOST")
-    elastic_port: str = Field(..., env="ELASTIC_PORT")
-    base_dir: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    redis_cache_time: int = Field(..., env="REDIS_CACHE_TIME")
+    redis_host: str = Field("127.0.0.1", env="REDIS_HOST")
+    redis_port: str = Field("6379", env="REDIS_PORT")
+    elastic_host: str = Field("127.0.0.1", env="ELASTIC_HOST")
+    elastic_port: str = Field("9200", env="ELASTIC_PORT")
 
     class Config:
-        env_file = '.env.api'
+        env_file = '.env'
         env_file_encoding = 'utf-8'
 
 
-settings = TestSettings()
+test_settings = TestSettings()
