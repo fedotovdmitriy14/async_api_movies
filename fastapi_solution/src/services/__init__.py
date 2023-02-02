8bb63c6b9248b0dd60957066fc122cfb31e97d66
dmitriyfedotov <fedotovdmitriy14@gmail.com>
2023-02-02 21:59:22 +0300

    added RedisStorage and AbstractStorage

---
+++ fastapi_solution/src/services/__init__.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from typing import Type
+
+from pydantic import BaseModel
+
+
+class AbstractStorage(ABC):
+    @abstractmethod
+    async def data_from_cache(self, index_name: str, model: [Type[BaseModel]], item_id: str) -> None:
+        pass
+
+    @abstractmethod
+    async def put_data_to_cache(self, index_name: str, model: dict) -> None:
+        pass
--- fastapi_solution/src/services/base.py
+++ fastapi_solution/src/services/base.py
@@ -11,6 +11,7 @@ from src.core.config import settings
 from src.models.film import FilmShort, FilmDetail
 from src.models.genre import Genre
 from src.models.person import PersonShort, Person
+from src.services import AbstractStorage
 
 Models = (FilmShort, FilmDetail, Genre, PersonShort, Person)
 
@@ -19,22 +20,7 @@ class BaseService:
     def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
         self.redis = redis
         self.elastic = elastic
-
-    async def _data_from_cache(self, index_name: str, model: Union[Models], item_id: str) -> Optional[Union[Models]]:
-        """пробуем получить запись по id из кеша"""
-        data = await self.redis.get(f'{index_name}::{item_id}')
-        if not data:
-            return None
-
-        return model.parse_raw(data)
-
-    async def _put_data_to_cache(self, index_name: str, model: dict):
-        """кладем запись в кеш"""
-        await self.redis.set(
-            f'{index_name}::{model["id"]}',
-            json.dumps(model),
-            expire=settings.redis_cache_time
-        )
+        self.redis_storage = RedisStorage(redis=self.redis)
 
     async def get_all(
             self,
@@ -91,7 +77,7 @@ class BaseService:
             index_name: str,
     ):
         """проверяется, есть ли запись в кеше; если нет - достается из эластика"""
-        res = await self._data_from_cache(index_name, model, id_)
+        res = await self.redis_storage.data_from_cache(index_name, model, id_)
         if not res:
             try:
                 res = await self.elastic.get(index=index_name, id=id_)
@@ -99,7 +85,28 @@ class BaseService:
                 raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=f'no {index_name} with this id')
             res = res['_source']
 
-            await self._put_data_to_cache(index_name, res)
+            await self.redis_storage.put_data_to_cache(index_name, res)
             return model(**res)
 
         return res
+
+
+class RedisStorage(AbstractStorage):
+    def __init__(self, redis: Redis):
+        self.redis = redis
+
+    async def data_from_cache(self, index_name: str, model: Union[Models], item_id: str) -> Optional[Union[Models]]:
+        """пробуем получить запись по id из кеша"""
+        data = await self.redis.get(f'{index_name}::{item_id}')
+        if not data:
+            return None
+
+        return model.parse_raw(data)
+
+    async def put_data_to_cache(self, index_name: str, model: dict) -> None:
+        """кладем запись в кеш"""
+        await self.redis.set(
+            f'{index_name}::{model["id"]}',
+            json.dumps(model),
+            expire=settings.redis_cache_time
+        )
