b27945e1e0071eecd01074f26a5cda3a5f5db375
Александр Карулин <av.karulin@tensor.ru>
2023-02-02 23:53:01 +0300

    tests structure

--- fastapi_solution/tests/functional/conftest.py
+++ fastapi_solution/tests/functional/conftest.py
@@ -1,18 +1,31 @@
 from typing import List
 
 import aiohttp
+import aioredis
 import pytest
 from elasticsearch import AsyncElasticsearch
 
 from tests.functional.settings import test_settings
 from tests.functional.utils.helpers import get_es_bulk_query
+from tests.functional.utils.wait_for_es import wait_for_es
+from tests.functional.utils.wait_for_redis import wait_for_redis
 
 
 @pytest.fixture(scope='session')
 async def es_client():
+    await wait_for_es()
     client = AsyncElasticsearch(hosts=[f'http://{test_settings.elastic_host}:{test_settings.elastic_port}'])
     yield client
     await client.close()
+
+
+@pytest.fixture(scope='session')
+async def redis_client():
+    await wait_for_redis()
+    client = await aioredis.create_redis_pool((test_settings.redis_host, test_settings.redis_port),
+                                              minsize=10, maxsize=20)
+    yield client
+    client.close()
 
 
 @pytest.fixture
@@ -36,11 +49,12 @@ async def client_session():
 
 @pytest.fixture
 def make_get_request(client_session):
-    url = f'{test_settings.elastic_host}:{test_settings.elastic_port}/api/v1/search'
-    query_data = {'search': 'The Star'}
-    session = await client_session()
-    async with session.get(url, params=query_data) as response:
-        body = await response.json()
-        headers = response.headers
-        status = response.status
-    return dict(body=body, headers=headers, status=status)
+    async def inner(method: str, params: dict):
+        url = f'http://{test_settings.elastic_host}:{test_settings.elastic_port}/api/v1/{method}'
+        session = await client_session()
+        async with session.get(url, params=params) as response:
+            body = await response.json()
+            headers = response.headers
+            status = response.status
+        return dict(body=body, headers=headers, status=status)
+    return inner
---
+++ fastapi_solution/tests/functional/utils/wait_for_redis.py
@@ -0,0 +1,14 @@
+import time
+
+import aioredis
+
+from tests.functional.settings import test_settings
+
+
+def wait_for_redis():
+    redis_client = await aioredis.create_redis_pool((test_settings.redis_host, test_settings.redis_port),
+                                                    minsize=10, maxsize=20)
+    while True:
+        if redis_client.ping():
+            break
+        time.sleep(1)
