35ebfbb7fc45683e1b899d2d1fba5eef4f09e7f6
dmitriyfedotov <fedotovdmitriy14@gmail.com>
2023-02-07 22:25:50 +0300

    moved search films tests to films

--- fastapi_solution/tests/functional/src/test_film.py
+++ fastapi_solution/tests/functional/src/test_film.py
@@ -4,7 +4,9 @@ import pytest
 
 from tests.functional.utils.helpers import make_get_request, es_write_data
 
-index = 'films'
+
+search_film_url_path = 'films/search/'
+film_url_path = 'films'
 data = [
     {
         "id": "65f01393-dd19-4b83-9703-d7764d33e489",
@@ -45,7 +47,7 @@ data = [
 
 @pytest.mark.asyncio
 async def test_get_all_films(client_session):
-    response = await make_get_request(client_session, method=index)
+    response = await make_get_request(client_session, method=film_url_path)
     assert response.get('status') == 200
     assert len(response.get('body')) > 1
 
@@ -55,7 +57,7 @@ async def test_get_all_films(client_session):
     '00af52ec-9345-4d66-adbe-50eb917f463a6', 'not_valid_uuid'
 ))
 async def test_get_film_not_valid(client_session, uuid):
-    response = await make_get_request(client_session, f'{index}/{uuid}')
+    response = await make_get_request(client_session, f'{film_url_path}/{uuid}')
     assert response.get('status') == 404
 
 
@@ -63,18 +65,43 @@ async def test_get_film_not_valid(client_session, uuid):
 async def test_get_film(es_client, client_session):
     await es_write_data(es_client, data, es_index='movies')
     film_id = data[0].get('id')
-    response = await make_get_request(client_session, method=f'{index}/{film_id}')
+    response = await make_get_request(client_session, method=f'{film_url_path}/{film_id}')
     assert response.get('status') == 200
     assert response.get('body')['id'] == film_id
 
 
 @pytest.mark.asyncio
 async def test_get_cache_film(client_session, es_client):
-    await es_write_data(es_client, data, index)
+    await es_write_data(es_client, data, film_url_path)
     film_id = data[0].get('id')
-    response_1 = await make_get_request(client_session, method=f'{index}/{film_id}')
+    response_1 = await make_get_request(client_session, method=f'{film_url_path}/{film_id}')
     assert response_1.get('status') == HTTPStatus.OK
     await es_client.delete('movies', film_id)
-    response_2 = await make_get_request(client_session, method=f'{index}/{film_id}')
+    response_2 = await make_get_request(client_session, method=f'{film_url_path}/{film_id}')
     assert response_2['status'] == HTTPStatus.OK
     assert response_1['body'] == response_2['body']
+
+
+@pytest.mark.asyncio
+async def test_get_all_films_search(client_session):
+    response = await make_get_request(client_session, method=search_film_url_path)
+    assert response.get('status') == 200
+    assert len(response.get('body')) > 1
+
+
+@pytest.mark.asyncio
+async def test_get_all_films_search_with_pagination(client_session):
+    params = {'page[size]': 1, 'page[number]': 1}
+    response = await make_get_request(client_session, method=search_film_url_path, params=params)
+    assert response.get('status') == 200
+    assert len(response.get('body')) == 1
+
+
+@pytest.mark.asyncio
+async def test_search_film_by_title(es_client, client_session):
+    params = {'query': 'test title'}
+    await es_write_data(es_client, data, es_index='movies')
+    film_id = data[0].get('id')
+    response = await make_get_request(client_session, method=f'{search_film_url_path}', params=params)
+    assert response.get('status') == 200
+    assert response.get('body')[0]['id'] == film_id
