4a2dfac1b4dbe9f893cfb0e35725cf99cfb79714
dmitriyfedotov <fedotovdmitriy14@gmail.com>
2023-02-09 23:07:57 +0300

    fixed mistake in create indices

--- fastapi_solution/tests/functional/utils/wait_for_es.py
+++ fastapi_solution/tests/functional/utils/wait_for_es.py
@@ -2,7 +2,7 @@ import time
 
 from elasticsearch import Elasticsearch
 
-from elastic_indexes import FILMS_INDEX, PERSONS_INDEX, GENRES_INDEX
+from tests.functional.elastic_indexes import FILMS_INDEX, PERSONS_INDEX, GENRES_INDEX
 from tests.functional.settings import test_settings
 
 
@@ -12,7 +12,9 @@ def wait_for_es():
         if es_client.ping():
             indices_dict = {'movies': FILMS_INDEX, 'persons': PERSONS_INDEX, 'genres': GENRES_INDEX}
             for index_name, index_dict in indices_dict.items():
-                if not es_client.exists(index=index_name):
+                if not es_client.indices.exists(index=index_name):
                     es_client.indices.create(index=index_name, ignore=400, body=index_dict)
             break
         time.sleep(1)
+
+wait_for_es()
