61f793ee73345fbbf1196910ef49c1c4e4efe1b3
dmitriyfedotov <fedotovdmitriy14@gmail.com>
2023-02-09 22:37:55 +0300

    split envs for local and docker tests; added creation of elastic indexes

--- fastapi_solution/tests/functional/utils/wait_for_es.py
+++ fastapi_solution/tests/functional/utils/wait_for_es.py
@@ -1,6 +1,8 @@
 import time
 
 from elasticsearch import Elasticsearch
+
+from elastic_indexes import FILMS_INDEX, PERSONS_INDEX, GENRES_INDEX
 from tests.functional.settings import test_settings
 
 
@@ -8,5 +10,9 @@ def wait_for_es():
     es_client = Elasticsearch(hosts=f'http://{test_settings.elastic_host}:{test_settings.elastic_port}')
     while True:
         if es_client.ping():
+            indices_dict = {'movies': FILMS_INDEX, 'persons': PERSONS_INDEX, 'genres': GENRES_INDEX}
+            for index_name, index_dict in indices_dict.items():
+                if not es_client.exists(index=index_name):
+                    es_client.indices.create(index=index_name, ignore=400, body=index_dict)
             break
         time.sleep(1)
